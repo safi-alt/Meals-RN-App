47885342cde77fd8f90d6b6c63fecf7f55d0a9fb
Safi Sohail Azim <safi@Safis-MacBook-Pro.local>
2021-03-08 18:15:33 +0500

    Navigation to the Meal detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoryMealsScreen = (props) => {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>The category meal screen Screen!</Text>
+      <Button
+        title="Go to Meals!!"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
     </View>
   );
 };
