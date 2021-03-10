b9c1a63700bbc1ef308d12cca6bc49fbc1b7d84a
Safi Sohail Azim <safiazim8@gmail.com>
2021-03-10 16:46:03 +0500

    Passing and reading Params on navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -19,7 +19,12 @@ const CategoriesScreen = (props) => {
       <TouchableOpacity
         style={styles.gridItem}
         onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
         }}
       >
         <View>
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>The category meal screen Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meals!!"
         onPress={() => {
